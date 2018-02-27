caf824851b53db0f52367f10d643c5f31c53120a
DESKTOP-7EES3O2\Emilie Rabeau <mie_rabeau@rogers.com>
2018-02-27 15:52:32 -0500

    up to step 7

--- DragosBandEmilieR/src/VarsAndExp/Program.cs
+++ DragosBandEmilieR/src/VarsAndExp/Program.cs
@@ -11,5 +11,20 @@ namespace VarsAndExp
         static void Main(string[] args)
         {
         }
+
+        /// <summary>
+        /// A function that will be used to convert Date, Minute, Seconds to Degrees 
+        /// </summary>
+        /// <param name="deg">Degrees</param>
+        /// <param name="min">Minutes</param>
+        /// <param name="sec">Seconds</param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        double Dms2Dd(int deg, int min, double sec, string bearing)
+
+        {
+            return 5000;
+        }
     }
+    
 }
