ad4b016015dd2d25c1563e12ebebbda907fc91b2
DESKTOP-7EES3O2\Emilie Rabeau <mie_rabeau@rogers.com>
2018-03-12 23:06:44 -0400

    VarsAndExps does not work

--- DragosBandEmilieR/src/VarsAndExp/Program.cs
+++ DragosBandEmilieR/src/VarsAndExp/Program.cs
@@ -8,11 +8,50 @@ namespace VarsAndExp
 {
     class Program
     {
+        /// <summary>
+        /// This program will convert DMS into Degrees, while taking the bearing (N,E,S,W) into consideration.
+        /// </summary>
+        /// <param name="deg">DEGREES</param>
+        /// <param name="min">MINUTES</param>
+        /// <param name="sec">SECONDS</param>
+        /// <param name="bearing"> BEARING is a string that contains the quadrant of the dms coordinate (i.e. N,S,E,W)</param>
+        /// <returns></returns>
+        /// 
+
         static void Main(string[] args)
         {
-            double dd = Dms2Dd(60, 30, 0, "N");
-            double expected = 1;
-            Console.WriteLine("Expected: {0} Actual: {1}", expected, actual);
+            int deg = 45;
+            int min = 20;
+            double sec = 5.6;
+            string bearing = "N";
+            double dd = Dms2Dd(deg, min, sec, bearing);
+            double expected = 45.4255;
+            Console.WriteLine("Expected: {0} Actual: {1}", expected, dd);
+
+            deg = 50;
+            min = 30;
+            sec = 4;
+            bearing = "E";
+            dd = Dms2Dd(deg, min, sec, bearing);
+            expected = -60.5;
+            Console.WriteLine("Expected: {0} Actual: {1}", expected, dd);
+
+            deg = 60;
+            min = 30;
+            sec = 4.5;
+            bearing = "S";
+            dd = Dms2Dd(deg, min, sec, bearing);
+            expected = 60.5;
+            Console.WriteLine("Expected: {0} Actual: {1}", expected, dd);
+
+            deg = 60;
+            min = 1;
+            sec = 1;
+            bearing = "W";
+            dd = Dms2Dd(deg, min, sec, bearing);
+            expected = 60.5;
+            Console.WriteLine("Expected: {0} Actual: {1}", expected, dd);
+            Console.ReadKey();
         }
         /// <summary>
         /// This program will convert DMS into Degrees, while taking the bearing (N,E,S,W) into consideration.
@@ -22,9 +61,32 @@ namespace VarsAndExp
         /// <param name="sec">SECONDS</param>
         /// <param name="bearing"> BEARING is a string that contains the quadrant of the dms coordinate (i.e. N,S,E,W)</param>
         /// <returns></returns>
-        static double Dms2Dd(int deg, int min, int sec, string bearing)
+        /// 
+        static double Dms2Dd(int deg, int min, double sec, string bearing) ///function
         {
-        return 1;
+            int swithValue;
+
+            bool positive; ///Business Rule #1 The first three parameters must be positive numbers
+            positive = (deg > 0 && min > 0 && sec > 0);
+
+            if (positive == false)
+            {
+                return 9999;
+            }
+            switch (bearing.ToUpper()) ///
+            {
+                case "N":
+                case "S":
+                    if (deg > 90) { return 9999; }
+                    break;
+                case "E":
+                case "W":
+                    if (deg > 180) { return 9999; }
+                    break;
+
+                default: return 9999;
+            }
+            return (deg + ((min + (sec / 60)) / 60)) * switchValue; /// conversion on equation
         }
     }
 }
